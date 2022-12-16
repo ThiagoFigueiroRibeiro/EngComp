cc47703dfd06300809cc3b456704ba5fe0f95fb3
ThiagoFigueiroRibeiro <thiago.figueiro.ribeiro@gmail.com>
2022-12-16 14:25:27 -0300

    Create TheCounter.c
    First code I've put on GitHub in a looong time. Hopefully one of many!

---
+++ 0_BeforeTheStorm/TheCounter.c
@@ -0,0 +1,16 @@
+// Baby's not so first code. But it'll do. 
+// Made 16/12/2022 by Myself
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+printf("Hello, I'm a counter\n");
+
+int n;
+
+for (n = 1; n < 11; n++){
+	printf("%d\n",n);
+} 
+
+return 0;
+}

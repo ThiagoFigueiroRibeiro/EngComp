ae5853f928a17cb94973781f630e44ff33b6ac52
ThiagoFigueiroRibeiro <thiago.figueiro.ribeiro@gmail.com>
2022-12-19 11:17:43 -0300

    Is fizz or it is buzz or it is fizzbuzz?
    My solution to the classic fizzbuzz problem solved in C.

---
+++ 0_BeforeTheStorm/fizzbuzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+
+int click;
+int i;
+printf ("Coloque ate qual numero voce deseja o fizzbuzz\n");
+
+// pegar o comando do teclado
+
+scanf("%d", &click);
+
+for (i = 1; i <= click; i++){
+printf ("%d",i);
+if (i % 15 == 0){ printf (" - FizzBuzz\n");}
+
+else if (i % 5 == 0){ printf (" - Buzz\n");}
+
+else if (i % 3 == 0){ printf (" - Fizz\n");}
+
+else { printf (" - \n");}
+}
+
+printf("Esse eh o fim do programa, uhuuuu\n");
+return 0;
+}
